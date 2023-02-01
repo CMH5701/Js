63ae2a79defc0030b0236d958e6c573decd5245d
최명환 <102547553+CMH5701@users.noreply.github.com>
2023-02-01 15:24:58 +0900

    Update Lotto.js

--- Lotto.js
+++ Lotto.js
@@ -1,9 +1,9 @@
 document.write('번호');
 //document.write('하이');//''
-//var name = '엄준식';
+//var name = '최명환';
 //document.write(typeof name);//불,정수,문자 무엇인지 타입 출력해줌
 var lotto = [];
-while (lotto.length < 6)/*배열안의 값이 6개가 될 때 까지*/ {
+while (lotto.length < 7)/*배열안의 값이 6+1개가 될 때 까지*/ {
     var num = parseInt(Math.random() * 45 + 1);//소수를 벌고 정수만
     if (lotto.indexOf(num) == -1){//lotto 배열에 num 값이 있는지 검사하는 코드를 작성해주세요.
         lotto.push(num);
